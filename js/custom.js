542c1e154d844afaee0a436209698184cb789d78
MohamedBod <40627636+MohamedBod@users.noreply.github.com>
2018-07-09 13:56:10 +0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+function info(){
+    var x = document.getElementById("info");
+    var y = document.getElementById("info-1");
+   var  z = document.getElementById("info-2");
+
+   x.style.display = "block";
+   y.style.display = "none";
+   z.style.display = "none";
+}
+
+function info1(){
+    var x = document.getElementById("info");
+    var y = document.getElementById("info-1");
+   var  z = document.getElementById("info-2");
+
+   x.style.display = "none";
+   y.style.display = "block";
+   z.style.display = "none";
+}
+
+
+function info2(){
+    var x = document.getElementById("info");
+    var y = document.getElementById("info-1");
+   var  z = document.getElementById("info-2");
+
+   x.style.display = "none";
+   y.style.display = "none";
+   z.style.display = "block";
+}
